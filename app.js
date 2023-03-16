7e2ae81f28ed6deba1138594388dece3922362f4
Omar Torres Urrego <omarurrego@192.168.0.5>
2023-03-15 20:48:27 -0500

    First commit, initial structure

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 8000;
+
+// Template engine running
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// Getting routes
+const authRoutes = require('./routes/auth.routes');
+
+app.use(authRoutes);
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+});
+server.on("error", (error) => console.log(`Error en servidor ${error}`));

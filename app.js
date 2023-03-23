3daf3ae4f8aca720b03dd0de7328fecfd3031cea
Omar Torres Urrego <omarurrego@192.168.0.5>
2023-03-22 19:58:24 -0500

    Added view Paz y Salvo

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(express.static(path.join(__dirname, "public")));
 const authRoutes = require("./routes/auth.routes");
 const toolsRoutes = require("./routes/tools.routes");
 
+//middleware for routes
 app.use(authRoutes);
 app.use(toolsRoutes);
 
